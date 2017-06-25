2ceac876e88fafeb3be397858363dc5b194caa88
Rodrigo Leite <rodrigo.ldasilva@hotmail.com>
2017-06-25 16:26:36 -0300

    Implementa RectSpace.receiveElem()
    Essa implementação garante que apenas os pontos que pertençam ao espaço sejam adicionados

--- teste_RectSpace.py
+++ teste_RectSpace.py
@@ -7,16 +7,42 @@ class Point2D():
 		return (self.x,self.y)
 
 class RectSpace():
+	elements = []
+	#==================================
+	#Confirma se está no espaço
+	def isinRange(self,point):
+		bound = self.getBounds()
+		x = point.getCoord()[0]
+		y = point.getCoord()[1]
+		return(((x>=bound[0][0])\
+				and(x<=bound[0][1]))\
+			and((y>=bound[1][0])\
+				and(y<=bound[1][1])))
 	
+	#==================================
+	#Assegura a iclusão se pontos que pertençam ao espaço
+	def receivePoints(self,elements):
+		print("In rect: x : "+str(self.getBounds()[0])+"; y : "+str(self.getBounds()[1]))
+		for elem in elements:
+			print("\t"),
+			if self.isinRange(elem):
+				self.elements.append(elem)
+				print("Point: "+str(elem.getCoord())+"was appended")
+			else:
+				print("Point: "+str(elem.getCoord())+"was NOT appended")
+			
+	#==================================
+	#Devolve ((x0,xf),(y0,yf))
+	def getBounds(self):
+		return((self.origin.getCoord()[0],self.origin.getCoord()[0]+self.Width),(self.origin.getCoord()[1],self.origin.getCoord()[1]+self.Height))
+		
+	#==================================
 	def __init__(self,elements=[],origin=Point2D((0.0,0.0)),size=(800.0,600.0)):
-		self.elements = elements
 		self.origin = origin
 		self.Width = size[0]
 		self.Height = size[1]
-	#==================================
-	#Assegura a iclusão se pontos que pertençam ao espaço
-	def insertPoints(self,elements):
-		pass
+		self.receivePoints(elements)
+	
 
 def testePoint2D(mode="empty",coordinates = (0.0,0.0)):
 	if mode=="empty":
