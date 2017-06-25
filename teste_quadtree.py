1532446aa3fad5e20ea5eb3dd005447fa471f079
Rodrigo Leite <rodrigo.ldasilva@hotmail.com>
2017-06-25 18:44:57 -0300

    Começa implementação
    mor_code é uma lista que armazena as tuplas que definem o morton code.
    A subdivisão de um Node já repassa aos filhos seus quadrantes

--- teste_quadtree.py
+++ teste_quadtree.py
@@ -7,13 +7,15 @@ class Node():
 	# In the case of a root node "parent" will be None. The
 	# "rect" lists the minx,minz,maxx,maxz of the rectangle
 	# represented by the node.
-	def __init__(self, parent, rect):
+	def __init__(self, parent, rect, quadrant):
 		self.parent = parent
 		self.children = [None,None,None,None]
 		if parent == None:
 			self.depth = 0
+			self.mor_code = []
 		else:
 			self.depth = parent.depth + 1
+			self.mor_code = parent.mor_code.append(quadrant)
 		self.rect = rect
 		x0,z0,x1,z1 = rect
 		if self.parent == None:
@@ -22,6 +24,7 @@ class Node():
 			self.type = Node.LEAF
 		else:
 			self.type = Node.BRANCH
+		print(mor_code)
 	#_______________________________________________________
 	# Recursively subdivides a rectangle. Division occurs 
 	# ONLY if the rectangle spans a "feature of interest".
@@ -31,14 +34,16 @@ class Node():
 		x0,z0,x1,z1 = self.rect
 		h = (x1 - x0)/2
 		rects = []
-		rects.append( (x0, z0, x0 + h, z0 + h) )
-		rects.append( (x0, z0 + h, x0 + h, z1) )
-		rects.append( (x0 + h, z0 + h, x1, z1) )
-		rects.append( (x0 + h, z0, x1, z0 + h) )
+		rects.append( (x0, z0, x0 + h, z0 + h) ) #00
+		rects.append( (x0 + h, z0, x1, z0 + h) ) #01
+		rects.append( (x0, z0 + h, x0 + h, z1) ) #10
+		rects.append( (x0 + h, z0 + h, x1, z1) ) #11
 		for n in range(len(rects)):
 			span = self.spans_feature(rects[n])
 			if span == True:
-				self.children[n] = self.getinstance(rects[n])
+				quadrant = (n/2,n%2)
+				print("\t"),
+				self.children[n] = self.getinstance(rects[n],quadrant)#Quadrant
 				self.children[n].subdivide() # << recursion
 	#_______________________________________________________
 	# A utility proc that returns True if the coordinates of
@@ -50,8 +55,8 @@ class Node():
 		return False
 	#_______________________________________________________
 	# Sub-classes must override these two methods.
-	def getinstance(self,rect):
-		return Node(self,rect)			
+	def getinstance(self,rect,quadrant):
+		return Node(self,rect,quadrant)			
 	def spans_feature(self, rect):
 		return False
 
